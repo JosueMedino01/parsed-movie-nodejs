5c1f861949135b65373bb5467acb9dd5718a01f0
Josué Medino <josuemedino5@gmail.com>
2025-07-03 15:48:23 -0300

    feat: add interfaces

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -11,3 +11,39 @@ export interface IMovie {
     maiorPremiacao: string;
     sinopse: string;
 }
+
+export interface IDetailMovieList {
+  filmes: IDetailMovie[]
+}
+
+export interface IDetailMovie {
+  titulo: string
+  diretor: string
+  ano: number
+  genero: string[]
+  duracao: number
+  ratings: Rating[]
+  elenco: string[]
+  sinopse: ISynopsis[]
+  poster: string
+  trailer: string
+  locacoes: string[]
+  orcamento: string
+  bilheteria: string
+  premios: IAward[]
+}
+
+export interface Rating {
+  valor: number
+  fonte: string
+}
+
+export interface ISynopsis {
+  texto: string
+  idioma: string
+}
+
+export interface IAward {
+  nome: string
+  relevancia: number
+}
